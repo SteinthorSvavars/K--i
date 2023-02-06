46e3c20af4b3830508e9a68190f268d37ef44eb5
SES <54535187+SteinthorSvavars@users.noreply.github.com>
2023-02-06 17:09:44 +0000

    verkefni3 klarad

--- Forritun_Önn_2/Geyma.cpp
+++ Forritun_Önn_2/Geyma.cpp
@@ -9,55 +9,33 @@
 #include<bits/stdc++.h>
 #include <string>
 #include <algorithm>
-#include <cctype>  
+#include <cctype>
+#include <new>   
 
 using namespace std;
 /*****   Classes   *****/
-class Kassi{
-    private: int lengd, breidd;
-    public:
-        void BoxSet(int, int);
-        int getVolume() {return (lengd*breidd)^2;}
-        int getArea() {return lengd*breidd;}
-};
-void Kassi::BoxSet(int a, int b){
-    lengd = a;
-    breidd = b;
-}
 
 /*****   Main   *****/
 int main()
 {
-    int run;
-    run = true;
-    Kassi Box;
-    do {
-        int Lidur;
-        cout << "\n1 Setja in tolur: " ; 
-        cout << "\n2 Rummal: " ;
-        cout << "\n3 Flatarmal: " ;
-        cout << "\n4 Haetta: " ;
-        cout << "\n Val er: ";
-        cin >> Lidur;
-        switch (Lidur) {
-        case 1:
-            int x, y;
-            cout << "Hvad er lengd? ";
-            cin >> x;
-            cout << "Hvad er breiddin? ";
-            cin >> y;
-            Box.BoxSet(x,y);
-            break;
-        case 2:
-            cout << "Rummal kassans er " << Box.getVolume();
-            break;
-        case 3:
-            cout << "Flatarmal kassans er " << Box.getArea();
-            break;
-        case 4:
-            run = false;   
+    int i,n, x;
+    int numbers[20];
+    int * p;
+        for (int f =0; f <20; f++) {
+            x = 101;
+            pointer1 = &numbers[f];
+            while(x > 100) { 
+                x = 1 + std::rand()/((RAND_MAX + 1u)/99); 
+                cout << x << ' ';
+                *p = x;
+            }
         }
+    cout << "You have entered: ";
+    for (n=0; n<20; n++){
+      cout << numbers[n] << ", ";
+    delete[] p;
     }
-    while (run == false);
     return 0;
 }
+  
+
--- Forritun_Önn_2/Verkefni_3.cpp
+++ Forritun_Önn_2/Verkefni_3.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
-#include<ctype.h>
-#include<stdio.h>
-#include<math.h>
-#include<iomanip>
-#include<cmath>
-#include<iostream>
-#include<iomanip>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <ctype.h>
+#include <stdio.h>
+#include <math.h>
+#include <iomanip>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <bits/stdc++.h>
 #include <string>
 #include <algorithm>
 #include <cctype>
 #include <cstdlib>
-#include <ctime>  
+#include <ctime>
+#include <new>  
 
 using namespace std;
 /*****   Classes   *****/
@@ -21,17 +22,26 @@ using namespace std;
 /*****   Main   *****/
 int main()
 {
-    int tala[20];
     int x;
-    for (int f=0; f != 20; f++) {
-        x = 101;
-        while(x > 100) 
-            x = 1 + std::rand()/((RAND_MAX + 1u)/99);  
-        std::cout << x << ' ';
-        tala[f] = x;
+    int fylki1[20], fylki2[20];
+    int * pointer1;
+    cout << "\nTolurnar i fylki1 eru: ";
+    for (int f =0; f <20; f++) {    
+            x = 101;
+            while(x > 100) { 
+                x = 1 + std::rand()/((RAND_MAX + 1u)/99); 
+                cout << x << ' ';   
+            }
+            fylki1[f] = x;
     }
-    for(int a = 1; a = 20; a++) {
-        std::cout << tala[a] << " ";
+    for (int f =0; f <20; f++) { 
+        pointer1 = &fylki2[f];
+        *pointer1 = fylki1[f];
+    }
+    cout << "\nTolurnar i fylki2 eru: ";
+    cout <<"";
+    for (int f =0; f <20; f++){
+        cout << fylki2[f] << " ";
     }
     return 0;
 }
